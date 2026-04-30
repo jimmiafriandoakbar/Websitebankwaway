c88a513746baf6e672be923ae34c3ffa90ffa423
jimmiafriandoakbar <jimmiafriandoakbar@gmail.com>
2026-04-30 17:10:03 +0700

    title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Bank Waway Lampung</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
