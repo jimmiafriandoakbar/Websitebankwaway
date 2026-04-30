11be02e1937b7294f0cdf1adc93a3c350c273931
jimmiafriandoakbar <jimmiafriandoakbar@gmail.com>
2026-04-30 17:21:43 +0700

    juduh title

--- resources/views/admin/partials/head.blade.php
+++ resources/views/admin/partials/head.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>{{ config('app.name', 'Bank Waway') }}</title>
+  <title>Bank Waway Lampung</title>
   <link rel="icon" href="{{asset('logo/LOGOputih.ico')}}">
 
   <!-- Google Font: Source Sans Pro -->
